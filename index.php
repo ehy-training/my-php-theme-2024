817408c8739175ec77376340ee26ce1b19fa5762
Enno Hyttrek <ehy.training@gmail.com>
2024-09-10 13:06:18 +0200

    Version 1.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?php bloginfo('title'); ?></title>
+    <?php wp_head() ?>
+</head>
+
+<body>
+<p>Dies ist das Index Template</p>
+    <h1><?php bloginfo('title'); ?></h1>
+
+    <?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+    ?>
+
+    <h2 class="my-post-title"><?php the_title(); ?></h2>
+
+    <?php
+        the_content();
+    endwhile;
+endif;
+?>
+
+
+    <?php wp_footer() ?>
+</body>
+
+</html>
