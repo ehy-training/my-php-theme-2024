05f54b1b502748b817c9af7cb78669cece69b41e
Enno Hyttrek <ehy.training@gmail.com>
2024-09-11 13:07:07 +0200

    Version: 1.1

--- page.php
+++ page.php
@@ -7,23 +7,38 @@
 </head>
 
 <body>
-<p>Dies ist das Page Template</p>
-    <h1><?php bloginfo('title'); ?></h1>
+    <header>
+        <div class="container">
+            <p>Dies ist das Page Template</p>
+            <h1><?php bloginfo('title'); ?></h1>
 
-    <?php
+            <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <?php
 if ( have_posts() ) :
     while ( have_posts() ) : the_post();
     ?>
 
-    <h2 class="my-post-title"><?php the_title(); ?></h2>
+            <h2 class="my-post-title"><?php the_title(); ?></h2>
 
-    <?php
+            <?php
         the_content();
     endwhile;
 endif;
 ?>
+        </div>
+    </main>
 
-
+    <footer>
+        <div class="container">
+                       <?php wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+            <p>© 2024 Enno Hyttrek</p>
+        </div>
+    </footer>
     <?php wp_footer() ?>
 </body>
 
